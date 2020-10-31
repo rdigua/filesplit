760dbf977e817fc201ca4e765407f26e2970ae4b
rdigua <digua@163.com>
2020-10-31 16:42:42 +0800

    feat:add config

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::collections::HashMap;
 
 pub struct format {
     pub delimiter: String,
@@ -22,13 +23,21 @@ impl format {
 
     pub fn from_file()->Self{
         let mut buf:String=String::new();
+        let mut h:HashMap<String,String>=HashMap.new();
         let f=File::open("config.toml");
         let f= match f{
             Ok(ff)=>f::read_to_string(),
             _ => println!("Tt is some errors."),
         } ;
-
+    let v:Vec<&str>=buf.split("\n").collect();
+        for i in v.iter(){
+            let s:String=i.chars().filter_map(|x| !x.split_whitespace()).collect();
+            unimplemented!();
+        }
         unimplemented!()
     }
 }
 
+fn vec_string_to_hushmap(s:String)->Result<HashMap<String,String>>{
+    unimplemented!();
+}
